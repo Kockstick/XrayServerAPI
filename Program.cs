b2a777904449d0d4b8738db3033e4cb5113cf068
Kockstik <kostyadi04@mail.ru>
2026-03-26 20:17:51 +0700

    Fixes prog

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.WebHost.UseUrls("http://127.0.0.1:5000");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,9 +19,11 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+app.UseForwardedHeaders();
 
 app.UseAuthorization();
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapControllers();
