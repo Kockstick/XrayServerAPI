0fbadc93a5089098cd9f22d9028925c630f8f7e8
Kockstik <kostyadi04@mail.ru>
2026-03-26 14:07:10 +0700

    Добавьте файлы проекта.

---
+++ Services/ApiKeyService.cs
@@ -0,0 +1,18 @@
+﻿namespace XrayServerAPI.Services;
+
+public class ApiKeyService
+{
+    public string ApiKey { get; }
+
+    public ApiKeyService()
+    {
+        var key = Environment.GetEnvironmentVariable("API_KEY");
+
+        if (string.IsNullOrWhiteSpace(key))
+        {
+            throw new Exception("API_KEY is not set in environment variables");
+        }
+
+        ApiKey = key;
+    }
+}
