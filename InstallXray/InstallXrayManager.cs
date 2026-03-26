57acd3ea84777ab305c5862d3446ee451a3b0758
Kockstik <kostyadi04@mail.ru>
2026-03-26 16:11:45 +0700

    Add XrayInstallManager

---
+++ InstallXray/InstallXrayManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace XrayServerAPI.InstallXray;
+
+public class InstallXrayManager
+{
+    public void Install()
+    {
+        try
+        {
+            var domain = "nl3.divpn.ru";
+            ExecBash(GetInstallCertProc(domain));
+            ExecBash(GetInstallXrayProc(domain));
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Error installing Xray: " + ex.Message);
+        }
+    }
+
+    private Process GetInstallCertProc(string domain)
+    {
+        var certScriptPath = Path.Combine(AppContext.BaseDirectory, "InstallXray", "InstallCert.bash");
+        Process instCert = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"{certScriptPath} {domain}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            }
+        };
+        return instCert;
+    }
+
+    private Process GetInstallXrayProc(string domain)
+    {
+        var scriptPath = Path.Combine(
+            AppContext.BaseDirectory,
+            "InstallXray",
+            "InstallXray.bash"
+        );
+
+        return new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"{scriptPath} {domain}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            }
+        };
+    }
+
+    private void ExecBash(Process process)
+    {
+        var errorBuilder = new StringBuilder();
+        process.OutputDataReceived += (sender, e) =>
+        {
+            if (e.Data != null)
+                Console.WriteLine(e.Data);
+        };
+
+        process.ErrorDataReceived += (sender, e) =>
+        {
+            if (e.Data != null)
+                Console.WriteLine("ERR: " + e.Data);
+            errorBuilder.AppendLine(e.Data);
+        };
+
+        process.Start();
+
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            throw new Exception(
+                $"Bash script failed with exit code {process.ExitCode}\n{errorBuilder}"
+            );
+        }
+    }
+}
