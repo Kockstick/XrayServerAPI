cb4a9ad8526be2a8d4cdffb519dfd0a872df6bda
Kockstik <kostyadi04@mail.ru>
2026-03-29 15:19:04 +0700

    Add property names

--- Xray/XrayData.cs
+++ Xray/XrayData.cs
@@ -1,10 +1,16 @@
-﻿namespace XrayServerAPI.Xray;
+﻿using System.Text.Json.Serialization;
+
+namespace XrayServerAPI.Xray;
 
 public class XrayData
 {
+    [JsonPropertyName("domain")]
     public string Domain { get; set; }
+    [JsonPropertyName("privateKey")]
     public string PrivateKey { get; set; }
+    [JsonPropertyName("hash32")]
     public string Hash32 { get; set; }   // PublicKey
+    [JsonPropertyName("shortId")]
     public string ShortId { get; set; }
 
     public string PublicKey => Hash32;
